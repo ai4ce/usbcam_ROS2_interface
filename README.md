176f19eef6a79839779c59cd390896e422253959
Irving Fang <zichuanfang2015@yahoo.com>
2024-07-15 11:24:54 -0400

    Initial push. Untested

---
+++ README.md
@@ -0,0 +1,70 @@
+# realsense_ROS2_interface
+
+This ROS2 package interfaces robots and/or joysticks with the official Intel RealSense ROS wrapper.
+
+While it's targeting Intel RealSense and Sony Playstation Joystick, the package can be easily modified to work with any USB camera and controller.
+
+For any questions, please contact the author, [Irving Fang](https://irvingf7.github.io/).
+
+This package is heavily influenced and inspired by a [similar ROS1 package](https://github.com/ripl/capture-images) written by [Dr. Takuma Yoneda](https://takuma.yoneda.xyz/) from TTIC.
+
+## Setup
+This package is only tested on `Ubuntu 22.04` and `ROS humble` distribution.
+
+It requires the official [RealSense ROS2 wrapper](https://github.com/IntelRealSense/realsense-ros) and the Intel RealSense SDK.
+
+I expect this package to be easily portable to other Ubuntu versions and ROS 2 distros, since it does not utilize anything specific to `Ubuntu 22.04` and `ROS humble`.
+
+
+
+## Installation
+
+After acquiring all the prerequisites, simply put this folder (which contains two ROS2 packages, technically speaking) inside your ROS workspace's `src` folder and run the following command in your ROS workspace.
+
+```
+colcon build
+```
+
+
+## Package Breakdown
+I will walk through the package structure briefly here. Feel free to skip over if you are familiar with how a typical ROS2 package works.
+
+This project is actually made of two ROS2 packages:
+
+- `realsense_capture`: contains all the code that actually does the job.
+
+- `realsense_interface_msg`: contains the definition for the service message type that's being used.
+
+It's a conventional ROS practice to dedicate an independent package for message type definitions.
+
+Inside `realsense_capture`:
+- `realsense_image_server.py`: responsible for constantly getting images from the camera, and is ready to serve the image whenever requested, that is, the capture button on the joystick is hit.
+
+- `realsense_image_client.py`: responsible for acquiring the image whenever the capture button on the joystick is hit. 
+
+
+## Usage
+To use the package, run
+
+```bash
+ros2 launch realsense_capture realsense_sys_launch.py
+```
+
+***Note***: The launch file also launches Foxglove, a visualization/monitoring app for ROS2. Get rid of related lines if desired. 
+
+There is no need to launch the RealSense ROS wrapper separately, as they are integrated into the launch file.
+
+### Parameters
+There are many parameters you can tune that come with RealSense, but some of the most important ones are:
+
+1. `camera_namespace` and `camera_name`: the topic name of your camera in ROS2 will be in the form of `camera_namespace/camera_name/xxx
+
+2. `save_folder`: indicates where to save the `.png` of the depth and color images.
+
+
+## Contributing
+
+Contributions to this project are welcome. If you encounter any issues or have suggestions for improvements, please open an issue on the GitHub repository.
+
+## Citation
+If you find this little package helpful, please consider citing it in your paper/work.
---
+++ usbsense_capture/launch/gelsight_sys_launch.py
@@ -0,0 +1,97 @@
+
+import os
+from flask import g
+import yaml
+
+from pathlib import Path
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import (
+    LaunchConfiguration,
+)
+
+from launch_ros.actions import Node
+
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+
+
+def declare_arguments():
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument("save_folder", default_value="/home/irving/Desktop", description="What folder to save the images to"),
+            DeclareLaunchArgument("config_name", default_value="gsmini", description="Name of the configuration file"),
+            DeclareLaunchArgument('final_config_path', default_value=[get_package_share_directory("gelsight_capture"), '/config/', LaunchConfiguration('config_name'), '.yaml'])
+        ]
+    )
+
+
+
+def load_yaml(package_name, file_name):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_name)
+
+    # try:
+    with open(absolute_file_path) as file:
+        return yaml.safe_load(file)
+    # except OSError:  # parent of IOError, OSError *and* WindowsError where available
+    #     return None
+    
+def generate_launch_description():
+
+    # ROS2 launch parameters cannot be directly access in the launch file, at least there is no easy way to do so.
+    # Therefore, we need to load the yaml file path here and pass it as a parameter to the node using a dummy launch parameter.
+    final_config_path = LaunchConfiguration("final_config_path")
+    gelsight_resource_path = os.path.join(get_package_share_directory("gelsight_capture"), "gelsight_resources")
+    save_folder_path = LaunchConfiguration("save_folder")
+
+    ld = LaunchDescription()
+    ld.add_entity(declare_arguments())
+    
+    foxglove_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('foxglove_bridge'), 
+                                    'launch', 
+                                    'foxglove_bridge_launch.xml')
+        )
+    )
+
+    
+    joy_launch = Node(
+                executable='joy_node',
+                package='joy',
+                name='joy_node',
+                parameters=[
+                    # {'autorepeat_rate': 50.0},
+                ],
+            )
+    image_server_launch = Node(
+        package='gelsight_capture',
+        executable='gelsight_image_server',
+        name='gelsight_image_server',
+        parameters=[{'config_path': final_config_path}],
+    )
+    pointcloud_server_launch = Node(
+        package='gelsight_capture',
+        executable='gelsight_pointcloud_server',
+        name='gelsight_pointcloud_server',
+        parameters=[{'config_path': final_config_path, 'resource_path': gelsight_resource_path}],
+    )
+    
+    gelsight_client_launch = Node(
+        package='gelsight_capture',
+        executable='gelsight_client',
+        name='gelsight_client',
+        parameters=[{'save_folder': save_folder_path}],
+    )
+    ld.add_action(foxglove_launch)
+    ld.add_action(joy_launch)
+    ld.add_action(image_server_launch)
+    ld.add_action(pointcloud_server_launch)
+    ld.add_action(gelsight_client_launch)
+
+    return ld
